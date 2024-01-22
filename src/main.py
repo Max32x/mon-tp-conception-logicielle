d650a2344ac9c407c02653b2ab90da703432028a
Jem'appelleSWAG DE POULE <id2321@ensai.fr>
2024-01-22 17:01:08 +0100

    prep exo 6 mais pas fini

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import datetime
 import pytz
 import logging
+from dotenv import load_dotenv
+import os
 
-logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
 
+load_dotenv()
+
+def get_fichier_sortie():
+    import sys
+    if len(sys.argv) > 1 :
+        return sys.argv[1]
+    return "traitement.log"
+
+
+logging.basicConfig(filename=get_fichier_sortie(), encoding='utf-8', level=logging.DEBUG)
 logging.info(f"Lancement du traitement")
 
 # Set the Réunion timezone
@@ -25,4 +36,5 @@ reunion_dt = utc_now.astimezone(reunion_tz)
 # Print out the current date and time in the Réunion timezone
 print("The current date and time in Réunion is:")
 print(reunion_dt.strftime("%Y-%m-%d %H:%M:%S"))
+
 
